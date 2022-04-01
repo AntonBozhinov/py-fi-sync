4969dee8da058370bbd7888c409e2a5e735fbb01
Anton Bozhinov <antonbozhinov@Antons-MacBook-Pro.local>
2022-04-01 14:02:10 +0300

    add server

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
+import os
+
 from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
+content_dir = os.path.join(os.getcwd(), "uploads")
+os.makedirs(content_dir, exist_ok=True)
+
 
 @app.post("/upload/")
 async def create_upload_file(file: UploadFile):
+    content = file.file.read()
+    with open(f"/{content_dir}/{file.filename}", "wb") as f:
+        f.write(content)
     return {"filename": file.filename}
